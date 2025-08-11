b8ddbd9c20d992247bc93f2e3c22bacd930515f8
SAID-SWIAAID <saidalmanani@gmail.com>
2025-08-11 18:17:58 +0100

    teybxh

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 3001;
 // =======================
 app.use(cors());
 app.use(express.json());
-
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 // =======================
 //  Start after DB Init
 // =======================
